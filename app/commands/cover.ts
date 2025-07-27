a6cfb41300b9e2df70bcfee4dc3f03d845375db2
yassir56069 <hoossanbukshyassir@gmail.com>
2025-07-27 21:05:33 +0400

    🧵 ugh, eslint

--- app/commands/cover.ts
+++ app/commands/cover.ts
@@ -282,7 +282,7 @@ async function handleUserScrobble(interaction: APIChatInputApplicationCommandInt
 
         const dominantColor = await getDominantColor(albumArtUrl);
         const baseUrl = getBaseUrl();
-        let iconUrl = 'https://www.last.fm/static/images/lastfm_avatar_twitter.52a5d69a85ac.png';
+        const iconUrl = 'https://www.last.fm/static/images/lastfm_avatar_twitter.52a5d69a85ac.png';
         if (dominantColor) {
             const hexColor = dominantColor.toString(16).padStart(6, '0');
             // iconUrl = `${baseUrl}/api/recolor-icon?color=${hexColor}`;
